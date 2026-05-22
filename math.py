9d808281e701f1c099f58715a3ac102206f01e03
Priyadharshan M <145208138+Darshan0403@users.noreply.github.com>
2026-05-22 22:16:11 +0530

    Test at may 22 for codebERT

---
+++ math.py
@@ -0,0 +1,7 @@
+def calculate_area(radius: float) -> float:
+    """Calculates the area of a circle."""
+    pi = 3.14159
+    return pi * (radius ** 2)
+
+def calculate_perimeter(radius: float) -> float:
+    return 2 * 3.14159 * radius
